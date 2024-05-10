4d4787ee233826494c3a575e0655a23277c99b42
Joseph Gazalet <49173224+jgaza@users.noreply.github.com>
2024-05-10 11:24:19 +0200

    feat: implement basic validation of book model fields
    Signed-off-by: Joseph Gazalet <49173224+jgaza@users.noreply.github.com>

--- UniversityLibrary/Models/Book.cs
+++ UniversityLibrary/Models/Book.cs
@@ -5,12 +5,15 @@ namespace UniversityLibrary.Models
     public class Book
     {
         public int ID { get; set; }
-        public string Title { get; set; }
+
+        [Required]
+        public string? Title { get; set; }
 
         [Display(Name = "ISBN")]
-        public string Isbn { get; set; }
+        public string? Isbn { get; set; }
 
         public int AuthorID { get; set; }
-        public string Publisher { get; set; }
+
+        public string? Publisher { get; set; }
     }
 }
