37f14db6797be1d2409ef6a8d73d6d517aa24236
Joseph Gazalet <49173224+jgaza@users.noreply.github.com>
2024-05-10 09:27:22 +0200

    refactor: use 'Display' attribute to specify the display format of book data
    Signed-off-by: Joseph Gazalet <49173224+jgaza@users.noreply.github.com>

--- UniversityLibrary/Models/Book.cs
+++ UniversityLibrary/Models/Book.cs
@@ -1,10 +1,15 @@
-﻿namespace UniversityLibrary.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityLibrary.Models
 {
     public class Book
     {
         public int ID { get; set; }
         public string Title { get; set; }
+
+        [Display(Name = "ISBN")]
         public string Isbn { get; set; }
+
         public int AuthorID { get; set; }
         public string Publisher { get; set; }
     }
