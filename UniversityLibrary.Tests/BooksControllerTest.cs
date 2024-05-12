5381cca3b4b16864429350d773ea1e31c18331ca
Joseph Gazalet <49173224+jgaza@users.noreply.github.com>
2024-05-12 23:09:59 +0200

    test: implement unit tests for displaying all book data
    Signed-off-by: Joseph Gazalet <49173224+jgaza@users.noreply.github.com>

--- UniversityLibrary.Tests/BooksControllerTest.cs
+++ UniversityLibrary.Tests/BooksControllerTest.cs
@@ -2,6 +2,7 @@ using UniversityLibrary.Controllers;
 using Moq;
 using Microsoft.AspNetCore.Mvc;
 using UniversityLibrary.Data.Repositories;
+using UniversityLibrary.Models;
 
 namespace UniversityLibrary.Tests;
 
@@ -16,5 +17,43 @@ public class BooksControllerTest
 		var result = await controller.Index();
 
 		Assert.IsType<ViewResult>(result);
+	}
+
+	[Fact]
+	public async Task Index_ReturnsAViewResult_WithAListOfAllBooks()
+	{
+		var repositoryMock = new Mock<IBooksRepository>();
+		repositoryMock.Setup(repo => repo.GetAllBooksAsync()).ReturnsAsync(InstantiateTestBooks());
+		var controller = new BooksController(repositoryMock.Object);
+
+		var result = await controller.Index() as ViewResult;
+		var model = result?.ViewData?.Model as IEnumerable<Book>;
+
+		Assert.Equal(InstantiateTestBooks().Count, model?.Count());
+	}
+
+	private List<Book> InstantiateTestBooks()
+	{
+		var books = new List<Book>
+		{
+			new Book()
+			{
+				ID = 1,
+				Title = "Test Title 2",
+				Isbn = "1234",
+				AuthorID = 1,
+				Publisher = "Test Publisher 1"
+			},
+			new Book()
+			{
+				ID = 2,
+				Title = "Test Title 1",
+				Isbn = "5678",
+				AuthorID = 2,
+				Publisher = "Test Publisher 2"
+			}
+		};
+
+		return books;
 	}
 }
