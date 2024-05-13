234d2b9001dd69e0cbc0fec1e736727b62846e01
Joseph Gazalet <49173224+jgaza@users.noreply.github.com>
2024-05-13 19:38:51 +0200

    feat: implement correct displaying of author and publisher information for books
    Signed-off-by: Joseph Gazalet <49173224+jgaza@users.noreply.github.com>

--- UniversityLibrary/Data/Repositories/BooksRepository.cs
+++ UniversityLibrary/Data/Repositories/BooksRepository.cs
@@ -3,42 +3,42 @@ using UniversityLibrary.Models;
 
 namespace UniversityLibrary.Data.Repositories
 {
-    public class BooksRepository(UniversityLibraryContext context) : IBooksRepository
-    {
-        private readonly UniversityLibraryContext _context = context;
+	public class BooksRepository(UniversityLibraryContext context) : IBooksRepository
+	{
+		private readonly UniversityLibraryContext _context = context;
 
-        public async Task<int> AddBookAsync(Book book)
-        {
-            _context.Add(book);
-            return await _context.SaveChangesAsync();
-        }
+		public async Task<int> AddBookAsync(Book book)
+		{
+			_context.Add(book);
+			return await _context.SaveChangesAsync();
+		}
 
-        public bool BookExists(int id)
-        {
-            return _context.Book.Any(e => e.ID == id);
-        }
+		public bool BookExists(int id)
+		{
+			return _context.Book.Any(e => e.ID == id);
+		}
 
-        public async Task<Book?> FindBookByIDAsync(int? id) => await _context.Book.FindAsync(id);
+		public async Task<Book?> FindBookByIDAsync(int? id) => await _context.Book.FindAsync(id);
 
-        public async Task<List<Book>> GetAllBooksAsync() => await _context.Book.ToListAsync();
+		public async Task<List<Book>> GetAllBooksAsync() => await _context.Book.Include(b => b.Authors).Include(b => b.Publisher).ToListAsync();
 
-        public async Task<Book?> GetBookByIDAsync(int? id) => await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+		public async Task<Book?> GetBookByIDAsync(int? id) => await _context.Book.Include(b => b.Authors).Include(b => b.Publisher).FirstOrDefaultAsync(m => m.ID == id);
 
-        public async Task<int> RemoveBookByIDAsync(int id)
-        {
-            var book = await _context.Book.FindAsync(id);
-            if (book != null)
-            {
-                _context.Book.Remove(book);
-            }
+		public async Task<int> RemoveBookByIDAsync(int id)
+		{
+			var book = await _context.Book.FindAsync(id);
+			if (book != null)
+			{
+				_context.Book.Remove(book);
+			}
 
-            return await _context.SaveChangesAsync();
-        }
+			return await _context.SaveChangesAsync();
+		}
 
-        public async Task<int> UpdateBookAsync(Book book)
-        {
-            _context.Update(book);
-            return await _context.SaveChangesAsync();
-        }
-    }
+		public async Task<int> UpdateBookAsync(Book book)
+		{
+			_context.Update(book);
+			return await _context.SaveChangesAsync();
+		}
+	}
 }
